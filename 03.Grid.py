d13f6a8f7fc0061d723992c147c42798fb92c7b3
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-12 16:51:23 +0530

    Grid

---
+++ 03.Grid.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root =Tk()
+
+mylab1= Label(root,text= "Hello Python")
+mylab2= Label(root,text= "Hello Python Coder")
+mylab1.grid(row=0, column=0)
+mylab2.grid(row=1, column=0)
+root.mainloop()
