603b7c98f4907b0beeaf550e45826f19dc957558
sarin cr <sarinrays@gmail.com>
2021-04-15 06:35:03 +0530

    ToCSV

--- 28.ToCSV.py
+++ 28.ToCSV.py
@@ -7,13 +7,13 @@ root= tk.Tk()
 canvas1 = tk.Canvas(root)
 canvas1.pack()
 
-def getCSV ():
+def ViewCSV ():
     global df
     location = filedialog.askopenfilename()
     df = pd.read_csv (location)
     print (df)
     
-btn = tk.Button(text=" Import CSV", command=getCSV)
+btn = tk.Button(text=" Import CSV", command=ViewCSV)
 canvas1.create_window(150, 150, window=btn)
 
 root.mainloop()
