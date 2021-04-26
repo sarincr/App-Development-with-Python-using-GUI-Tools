eacbbd4015f5095dca9ce9bd975ea5b53e59ae31
sarincr <sarinrays@gmail.com>
2021-04-26 10:57:18 +0530

    First Menu

--- 36.Notepad.py
+++ 36.Notepad.py
@@ -11,6 +11,14 @@ main_menu = tks.Menu()
 file = Menu(main_menu, tearoff = False)
 main_menu.add_cascade(label= "File", menu = file)
 
+file.add_command(label ="New", compound = tks.LEFT,  accelerator = "Ctrl +N")
+file.add_command(label ="Open", compound = tks.LEFT,  accelerator = "Ctrl +O")
+file.add_command(label ="Save", compound = tks.LEFT,  accelerator = "Ctrl +S")
+file.add_command(label ="Save as", compound = tks.LEFT,  accelerator = "Ctrl +NS")
+file.add_command(label ="Exit", compound = tks.LEFT,  accelerator = "Ctrl +Esc")
+
+
+
 tk.config(menu=main_menu)
 
 tk.mainloop()
